6b1e641914ef1a5bfa52872a466cb3029690bd9c
Akash-Kharat <akashkharat1339@gmail.com>
2022-11-05 23:22:25 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-// import React from 'react'
-import {ChatEngine} from 'react-chat-engine';
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 import './App.css';
-import ChatFeed from './components/ChatFeed'
-function App() {
+
+const projectID = 'c0a2ef2f-72db-44f2-ba43-5f444a8bfa2d';
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
   return (
     <ChatEngine
-        height="100vh"
-        projectID="c0a2ef2f-72db-44f2-ba43-5f444a8bfa2d"
-        userName="microengineer"
-        userSecret="12345"
-        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/>}
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+       
+      userSecret={localStorage.getItem('password')}
+      
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
     />
-  )
-}
+  );
+};
+
+// infinite scroll, logout, more customizations...
 
 export default App;
-
